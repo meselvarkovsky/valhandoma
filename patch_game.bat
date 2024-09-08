5aa60aebda611a5dcff6e97d504a4cbfa29088f9
None <None>
2024-09-09 00:19:34 +0300

    save progress

--- patch_game.bat
+++ patch_game.bat
@@ -1,2 +1,2 @@
-cp /y .\Nyandoma.db %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.db
-cp /y .\Nyandoma.fwl %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.fwl
+copy /y .\Nyandoma.db %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.db
+copy /y .\Nyandoma.fwl %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.fwl
--- save_to_repo.bat
+++ save_to_repo.bat
@@ -1,5 +1,5 @@
-cp /y %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.db .\Nyandoma.db
-cp /y %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.fwl .\Nyandoma.fwl
+copy /y %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.db .\Nyandoma.db
+copy /y %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.fwl .\Nyandoma.fwl
 
 git add .
 git commit -m "save progress"
