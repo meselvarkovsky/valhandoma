b0de3683d6ed3533a2dbc53f5280eea7cdf375f7
None <None>
2024-09-09 00:05:54 +0300

    save progress

---
+++ init.bat
@@ -0,0 +1,6 @@
+set USER_NAME=None
+set USER_EMAIL=None
+
+git config user.name %USER_NAME%
+git config user.email %USER_EMAIL%
+
---
+++ patch_game.bat
@@ -0,0 +1,2 @@
+cp /y .\Nyandoma.db %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.db
+cp /y .\Nyandoma.fwl %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.fwl
