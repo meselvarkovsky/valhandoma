c11a86f70dd3886f8ca9cbcadd5680ffee72ba31
None <None>
2024-09-09 00:14:53 +0300

    save progress

--- save_to_repo.bat
+++ save_to_repo.bat
@@ -1,3 +1,6 @@
+cp /y %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.db .\Nyandoma.db
+cp /y %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds\Nyandoma.fwl .\Nyandoma.fwl
+
 git add .
 git commit -m "save progress"
 git push
